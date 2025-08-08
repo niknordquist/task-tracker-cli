af675caab762738529a29d422dd770b821ca77d0
Nik Nordquist <nnordquist3@gmail.com>
2025-08-08 15:00:34 -0500

    Add new task to local JSON file

---
+++ main.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+import os
+
+totalTasks = 0
+        
+def add(description):
+    with open("todo.json", "w") as file:
+        try:
+            file.write(f'{totalTasks + 1},{description},{datetime.now()},{datetime.now()}')
+        except Exception as e:
+            print(f'Failed to create new task. Error: {e}')
+        
+        
+def welcomeMessage():
+    print("Hello, welcome to the Task Tracker Command Line Prompt!")
+    print("""Commands:\n\nadd <task info>\ndelete <task id>\nupdate <task id> <task info>
+mark-in-progress <task id>\nmark-done <task id>\nlist done/todo/in-progress\nPress 'q' to quit\n""")
+
+def main():
+    welcomeMessage()
+    endTrigger = False
+    while endTrigger == False:
+        userInput = input("Enter a command (Press 'q' to quit): ")
+        currentCommand = userInput.split()
+        match currentCommand[0]:
+            case "add":
+                add(currentCommand[1])
+            case 'q':
+                endTrigger = True
+            case _:
+                print("Invalid command!")
+        
+
+if __name__ == "__main__":
+    main()
+     
