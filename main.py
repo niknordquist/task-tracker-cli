22a67f75c78b05ee723c991374f769bf4f167e10
Nik Nordquist <nnordquist3@gmail.com>
2025-08-09 09:42:20 -0500

    Allow deletion of tasks

--- main.py
+++ main.py
@@ -1,29 +1,45 @@
 from datetime import datetime
 import json
 
-def load_file(file):
-    tasks_file = json.load(file)
-    task_list = tasks_file["tasks"]
-    file.seek(0)
+def getNextID():
+    with open("id.txt", "r+") as file:
+        id_file = json.load(file)
+        file.seek(0)
+        id = int(id_file)
+        id += 1
+        json.dump(id, file)
+        return id 
         
 def add(description):
     with open("todo.json", "r+") as file:
         try:
-            load_file(file)
+            tasks_file = json.load(file)
+            file.seek(0)
             data = {
-                'id': len(task_list) + 1,
+                'id': getNextID(),
                 'description': description,
                 'status': "Todo",
                 'createdAt': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
                 'updatedAt': datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             }
-            task_list.append(data)
+            tasks_file.append(data)
             json.dump(tasks_file, file)
+            print(f'Task: {description} with ID {data["id"]} added!')
         except Exception as e:
             print(f'Failed to create new task. Error: {e}')
             
 def delete(id):
-    pass
+    global totalTasks
+    with open("todo.json", "r+") as file:
+        try:
+            tasks_file = json.load(file)
+            file.seek(0)
+            tasks_file = [val for val in tasks_file if val['id'] != id]
+            json.dump(tasks_file, file)
+            file.truncate()
+            print(f'Deleted task with ID {id}!')
+        except Exception as e:
+            print(f'Failed to delete task with id {id}: {e}')
 
 def update(id, description):
     pass
@@ -54,13 +70,13 @@ def main():
                 case "add":
                     add(currentCommand[1])
                 case "delete":
-                    delete(currentCommand[1])
+                    delete(int(currentCommand[1]))
                 case "update":
-                    update(currentCommand[1], currentCommand[2])
+                    update(int(currentCommand[1]), currentCommand[2])
                 case "mark-in-progress":
-                    mark_in_progress(currentCommand[1])
+                    mark_in_progress(int(currentCommand[1]))
                 case "mark-done":
-                    mark_done(currentCommand[1])
+                    mark_done(int(currentCommand[1]))
                 case "list":
                     list_tasks() if len(currentCommand) < 2 else list_tasks(currentCommand[1])
                 case 'q':
@@ -69,6 +85,8 @@ def main():
                     print("Invalid command!")
         except IndexError as ioe:
             print("Command failed, please provide the correct format in your command.")
+        except ValueError as ve:
+            print("Command failed, task ID must be an integer")
 
 if __name__ == "__main__":
     main()
